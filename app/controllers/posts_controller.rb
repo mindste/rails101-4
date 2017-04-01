9593d42f740bf39f4c9740a03577d45be836a96b
robote <2233628310@qq.com>
2017-04-01 18:18:33 +0800

    implement  My Posts  edit&destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,35 @@ class PostsController < ApplicationController
         redirect_to   group_path(@group)
      else
        render  :new
-     end 
+     end
+   end
+
+   def   edit
+     @group  =  Group.find(params[:group_id])
+     @post   =  Post.find(params[:id])
+     @post.group   =  @group
+   end
+
+   def   update
+      @group  =  Group.find(params[:group_id])
+      @post  = Post.find(params[:id])
+      @post.group  =  @group
+
+      if  @post.update(post_params)
+        redirect_to   account_posts_path, notice: "Post  Updated!"
+      else
+        render  :edit
+      end
+   end
+
+
+   def    destroy
+      @group  =  Group.find(params[:group_id])
+      @post  =  Post.find(params[:id])
+      @post.group    =  @group
+      @post.destroy
+
+      redirect_to  account_posts_path,  warning: "Post  deleted!"
    end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
    end
 
    root  'groups#index'
-
+  
 end
